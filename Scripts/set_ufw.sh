d51892f0543ea5c0ccc86e2ef1d602b89564f776
jcloud-devops <36216603+jcloud-devops@users.noreply.github.com>
2018-02-21 16:39:06 +0900

    Update set_ufw.sh

--- Scripts/set_ufw.sh
+++ Scripts/set_ufw.sh
@@ -1,6 +1,10 @@
 sudo ufw status numbered
 sudo ufw allow from 10.0.0.0/24
 sudo ufw allow ssh
+sudo ufw allow from 114.70.193.174
+sudo ufw allow from 114.70.193.175
+sudo ufw allow from 114.70.193.176
+
 #sudo ufw allow 3932/tcp
 #sudo ufw allow 3932/udp
 sudo ufw status numbered
