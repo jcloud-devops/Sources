aa04cf3785882117170357383c0ba7b15d21a651
jdevops <jcloud.devops@gmail.com>
2018-02-21 11:15:04 +0900

    Modification for ufw setup //hcpark

--- Scripts/set_ufw.sh
+++ Scripts/set_ufw.sh
@@ -1,5 +1,6 @@
 sudo ufw status numbered
 sudo ufw allow from 10.0.0.0/24
-sudo ufw allow 3932/tcp
-sudo ufw allow 3932/udp
+sudo ufw allow ssh
+#sudo ufw allow 3932/tcp
+#sudo ufw allow 3932/udp
 sudo ufw status numbered
